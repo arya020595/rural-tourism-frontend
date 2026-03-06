9e6bead5619417be005855c37c574283d12897a1
FachriPermadi <hermansigue@gmail.com>
2026-03-06 09:55:38 +0700

    Applied component header association and company for operator menu

--- src/app/operator-bookings/operator-bookings.module.ts
+++ src/app/operator-bookings/operator-bookings.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { OperatorBookingsPageRoutingModule } from './operator-bookings-routing.module';
 
+import { SharedModule } from '../_shared/shared.module';
 import { OperatorBookingsPage } from './operator-bookings.page';
 
 @NgModule({
@@ -13,7 +14,8 @@ import { OperatorBookingsPage } from './operator-bookings.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    OperatorBookingsPageRoutingModule
+    OperatorBookingsPageRoutingModule,
+    SharedModule,
   ],
   declarations: [OperatorBookingsPage]
 })
