9208baf4b8265c2a9d74ad3681b00188ffc9d205
Meezan_2108 <hamizanalimarjafri2108@gmail.com>
2026-03-09 08:53:20 +0800

    Apply new Role and Pages "Association"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -221,11 +221,26 @@ const routes: Routes = [
       import('./operator-bookings/operator-bookings.module').then(
         (m) => m.OperatorBookingsPageModule,
       ),
-  },  {
-    path: 'activity-and-accommodation-management',
-    loadChildren: () => import('./activity-and-accommodation-management/activity-and-accommodation-management.module').then( m => m.ActivityAndAccommodationManagementPageModule)
   },
-
+  {
+    path: 'activity-and-accommodation-management',
+    loadChildren: () =>
+      import('./activity-and-accommodation-management/activity-and-accommodation-management.module').then(
+        (m) => m.ActivityAndAccommodationManagementPageModule,
+      ),
+  },
+  {
+    path: 'association/dashboard',
+    loadChildren: () =>
+      import('./association/dashboard/dashboard.module').then(
+        (m) => m.DashboardPageModule,
+      ),
+  },
+  {
+    path: 'association/login',
+    loadChildren: () =>
+      import('./association/login/login.module').then((m) => m.LoginPageModule),
+  },
 ];
 
 @NgModule({
